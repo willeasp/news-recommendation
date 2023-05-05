0b8b0b57c3e0fe10e37098387873b00f9e39876b
pjoe <pjoe@kth.se>
2023-05-05 11:21:13 +0200

    integrated backend and frontend

--- frontend/src/NewsSearch.js
+++ frontend/src/NewsSearch.js
@@ -33,8 +33,8 @@ const NewsSearch = () => {
       }
     });
 
-    console.log(res.data);
     setRelevance(Array.apply(null, Array(res.data.hits.hits.length)).map(_ => false))
+    console.log(res.data.hits.hits);
     setResults(res.data.hits.hits);
     
   }
@@ -42,8 +42,6 @@ const NewsSearch = () => {
   const expand = (idx) => {
 
     const art = document.querySelectorAll(".textDiv")
-
-    console.log(art);
     
     var nr = 0;
     art.forEach((_) => {
@@ -61,34 +59,21 @@ const NewsSearch = () => {
     setOpen(idx)
   }
 
-  /*
-  useEffect(() => {
-    
-    const searchMovies = async () => {
-      console.log("searchMovies")
-      const res = await axios.get("http://localhost:5000/search", {
-        proxy: {
-          host: 'cors-proxy.com',
-        },
-        params: {
-          name: name,
-          actors: actors,
-          genre: genre,
-          date: date
-        }
-      });
-      setMovies(res.data.hits.hits);
-    }
+  const moreLikeThis = async (idx) => {
 
-    if (name || actors || genre || date) {
-      searchMovies();
-    } else {
-      setMovies([]);
-    }
-    
-  }, [name, actors, genre, date]);
+    console.log(`Want more articles which are like: ${results[idx]._source.title}`);
+    const res = await axios.get("http://localhost:5001/recommend", {
+      params: {
+        title: results[idx]._source.title,
+        text: results[idx]._source.text,
+      }
+    });
 
-  */
+    setRelevance(Array.apply(null, Array(res.data.hits.hits.length)).map(_ => false))
+    setResults(res.data.hits.hits);
+
+  }
+
   return (
     <div className="container">
       <form>
@@ -104,57 +89,15 @@ const NewsSearch = () => {
         {results.map((item, idx) => { return (
           <div key={idx} className="article">
             <h1>{item._source.title}</h1>
-            <p>{item._source.abstract}</p>
+            <p>{item._source.text.replace("NYHETER", "").replace("Av:", "").trim()}</p>
             <div className="textDiv" id={`text-${idx}`}>
               <div className="text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis tortor 
-                ut tempus volutpat. Praesent vel dapibus mauris. Fusce facilisis augue non elit 
-                volutpat, commodo finibus risus facilisis. Suspendisse lacus orci, tristique nec 
-                maximus in, condimentum sed odio. Nunc eu condimentum leo. Cras euismod orci sit 
-                amet porta luctus. Integer pellentesque venenatis sapien fringilla placerat. 
-                Pellentesque finibus turpis eu rutrum rutrum. Sed in sem viverra, luctus mauris i
-                d, placerat mauris. Donec odio nisl, venenatis vestibulum eros vitae, fermentum 
-                bibendum lorem. Nullam sit amet viverra quam, at lacinia sem. Proin eget ante arcu. 
-                Aliquam quis cursus orci, ut fringilla diam. Sed orci massa, imperdiet eu semper vitae,
-                ullamcorper sed lorem. Nulla ac rutrum lorem. Proin sed orci ut mi euismod rhoncus. 
-                Quisque leo erat, egestas sit amet facilisis in, malesuada eget urna. Nam iaculis 
-                tristique nisi vehicula elementum. Pellentesque nec tincidunt augue. Vivamus faucibus 
-                risus ac rhoncus venenatis. Fusce rutrum ligula lacus, ac sodales felis ullamcorper vel. 
-                Nullam sit amet aliquet mauris. Vestibulum accumsan a nulla et convallis. Sed pulvinar est 
-                ipsum, sed ullamcorper nibh dapibus consequat. Nam mattis augue eu velit scelerisque 
-                eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos 
-                himenaeos. Proin interdum risus viverra magna rutrum vulputate. Donec eleifend orci hendrerit 
-                lacus efficitur dapibus. Morbi consequat ex maximus eleifend ornare. Nulla a lobortis felis.
-                Nulla non ipsum ac erat tincidunt mattis. Sed feugiat id sem id condimentum. Sed tempus, dui 
-                a interdum ornare, justo nibh vehicula dui, quis interdum nisi turpis vitae erat. In sit amet 
-                justo et nisl blandit finibus. Nullam vulputate, metus ac finibus fringilla, dui eros 
-                pretium purus, id volutpat dolor arcu id sem. Pellentesque tempus, felis sed volutpat 
-                ultricies, ante turpis iaculis ante, ut pharetra mauris odio ac libero. Donec accumsan 
-                tristique lacus ultricies blandit. Nunc at purus rutrum, varius nisl vel, eleifend nisi. 
-                Donec convallis sem nec metus aliquam, a porttitor urna efficitur. Curabitur tempus 
-                neque diam, in pulvinar turpis ornare ac. Nulla facilisi. Sed cursus lorem sit amet 
-                dolor commodo mattis. Mauris faucibus, lorem et egestas congue, eros ligula rutrum 
-                sapien, ut scelerisque ex massa ut lectus. Cras eget lectus accumsan, condimentum dolor 
-                varius, aliquet leo. Maecenas nibh nisi, venenatis id metus ut, vulputate sagittis 
-                lectus. Aliquam aliquet tellus tortor, at mattis odio finibus nec. Pellentesque nec 
-                massa erat. Donec ex ligula, iaculis sit amet nunc in, commodo ullamcorper mauris. 
-                Pellentesque congue purus quis massa auctor dictum. Aenean mollis in metus quis 
-                rhoncus. Sed pharetra ultricies tristique. Sed ut est ut odio cursus vulputate eget 
-                sed velit. Fusce scelerisque vel tellus sed bibendum. Nullam ut tempus libero, id 
-                porttitor turpis. Nullam lacinia rhoncus quam, tempus pharetra quam bibendum ac. 
-                Nunc pharetra consequat justo, et sagittis arcu fringilla ut. Curabitur ac diam vel 
-                tellus hendrerit porttitor. Sed consectetur sem tellus, et auctor erat tincidunt sed. 
-                Cras malesuada erat at blandit pretium. Morbi egestas vitae urna nec commodo. 
-                Donec velit augue, ornare ut condimentum ac, gravida sit amet diam. Phasellus faucibus 
-                accumsan dolor, quis ultricies elit. Etiam sed tortor luctus, euismod sem nec, tristique 
-                lacus. Aliquam fringilla, ex non malesuada aliquam, dui leo bibendum nibh, ut varius mi 
-                nisi vel arcu. Etiam est libero, euismod id arcu id, tincidunt blandit mauris. Sed nec 
-                ullamcorper turpis, at varius justo. Donec vulputate interdum massa, elementum semper felis. 
+                {item._source.text.replace("NYHETER", "").replace("Av:", "").trim()}
               </div>
             </div>
             <div className="buttons">
               {open === idx ? <button onClick={() => expand(-1)}>Läs mindre</button> : <button onClick={() => expand(idx)}>Läs mer</button>}
-              <div className="checkbox" onClick={() => {checkbox(`article-${idx}`); toggleRelevance(idx)}}>
+              <div className="checkbox" onClick={() => {moreLikeThis(idx)}}>
                 <div id={`article-${idx}`}></div>
                 Mer som detta
               </div>        
@@ -164,45 +107,6 @@ const NewsSearch = () => {
       </div>
     </div>
   )
-
-  /*
-  return (
-    <div>
-      <form>
-        <label>
-          Name:
-          <input type="text" value={name} onChange={event => setName(event.target.value)} />
-        </label>
-        <br />
-        <label>
-          Actors:
-          <input type="text" value={actors} onChange={event => setActors(event.target.value)} />
-        </label>
-        <br />
-        <label>
-          Genre:
-          <input type="text" value={genre} onChange={event => setGenre(event.target.value)} />
-        </label>
-        <br />
-        <label>
-          Release Date:
-          <input type="text" value={date} onChange={event => setDate(event.target.value)} />
-        </label>
-      </form>
-      <div>
-        {movies.map(movie => (
-          <div key={movie._id}>
-            <img src={movie._source.poster_url} alt={movie._source.name} />
-            <h2>{movie._source.name}</h2>
-            <p>Actors: {movie._source.actors}</p>
-            <p>Genre: {movie._source.genre}</p>
-            <p>Release Date: {movie._source.release_date}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-  */
 }
 
 
